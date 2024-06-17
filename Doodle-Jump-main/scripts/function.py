4378da3bf57cf428497c512d4b9e67cdee520cfa
Timur2345 <131990454+Timur2345@users.noreply.github.com>
2024-06-17 19:36:09 +0300

    Delete Doodle-Jump-main directory

--- Doodle-Jump-main/scripts/function.py
+++
@@ -1,7 +0,0 @@
-import pygame 
-import os
-def load_image(*paths):
-    path = os.path.join(*paths)
-    image = pygame.image.load(path).convert()
-    image.set_colorkey((0, 0, 0))
-    return image
