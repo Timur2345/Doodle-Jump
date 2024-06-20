39863b766d13490d509a60ca4a29c89f7c25ac59
Timur2345 <131990454+Timur2345@users.noreply.github.com>
2024-06-20 17:55:30 +0300

    Add files via upload

--- DoodleJump.py
+++ DoodleJump.py
@@ -1,12 +1,13 @@
 import pygame
 from scripts.app import App
 
+
 def main():
     pygame.init()
+
     app = App()
     app.run()
-    pygame.quit()
 
-#точка входа
+
 if __name__ == "__main__":
     main()
---
+++ scripts/game.py
@@ -0,0 +1,44 @@
+import pygame
+from scripts.constants import display_size
+from scripts.function import load_image
+from scripts.sprite import Sprite
+from scripts.player import Player
+class Game():
+    def __init__(self):
+        self.background = load_image("assets", "images", "background.png")
+        self.platforms = [
+            Sprite(load_image("assets","images","platform.png"), [240,700]),
+            Sprite(load_image("assets","images","platform.png"), [240,450]),
+            Sprite(load_image("assets","images","platform.png"), [240,200]),
+        ]
+        self.player = Player(
+            load_image("assets","images","player.png"),
+            (240,600),
+            20,
+            10,
+            0.75,
+        )
+        self.offset_y=0
+
+    def render_objects(self,scene):
+        scene.blit(self.background, (0,0))
+        for platform in self.platforms:
+            platform.render(scene, self.offset_y)
+        self.player.render(scene, self.offset_y)
+    def process_key_down_event(self,key):
+        if key == pygame.K_a:
+            self.player.is_walking_left = True
+        if key == pygame.K_d:
+            self.player.is_walking_right = True
+    def process_key_up_event(self,key):
+        if key == pygame.K_a:
+            self.player.is_walking_left = False
+        if key == pygame.K_d:
+            self.player.is_walking_right = False
+    def update_objects(self):
+        for platform in self.platforms:
+            if self.player.collide(platform.rect):
+                self.player.on_platform = True
+        self.player.update()
+        if self.player.rect.bottom - self.offset_y < display_size[1] / 3:
+            self.offset_y = self.player.rect.bottom - display_size[1] / 3
